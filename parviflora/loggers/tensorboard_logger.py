3d9d43944d8daaf74d5454987c5bddfb5b4bd091
Pawel Gajewski <lubiluk@gmail.com>
2023-04-07 18:59:15 +0200

    logger save dir

--- parviflora/loggers/tensorboard_logger.py
+++ parviflora/loggers/tensorboard_logger.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Optional
 
 from torch.utils.tensorboard import SummaryWriter
@@ -6,6 +7,10 @@ from .base_logger import BaseLogger
 
 
 class TensorboardLogger(BaseLogger):
+    def __init__(self, save_dir: Path) -> None:
+        super().__init__()
+        self.save_dir = save_dir
+
     def __enter__(self):
         self.open()
         return self
@@ -20,7 +25,7 @@ class TensorboardLogger(BaseLogger):
         self.writer.add_scalar(name, value, step)
 
     def open(self) -> None:
-        self.writer = SummaryWriter()
+        self.writer = SummaryWriter(log_dir=self.save_dir)
 
     def close(self) -> None:
         self.writer.flush()
